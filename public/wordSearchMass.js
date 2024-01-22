10fb186004daf31244894afb1af49ecaeb9cdcd6
unknown <campbell.nilsen@gmail.com>
2024-01-22 14:38:31 -0500

    a few checks on the searcher

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -80,8 +80,12 @@ function seeAllWords(resultDiv, searchString, searchSetting) {
         resultDiv.innerText = ""
         for (let item of res) {
             let wordCheck = checkWord(item.word, searchString, searchSetting);
-            if (wordCheck[0]) {
-                resultDiv.appendChild(getAllWordTokens(searchString, wordCheck[1], item.total_count, item.verse_addresses, item.verse_tokens));
+            if (wordCheck[0] && searchString != "") {
+                if (wordCheck[1].length == 0) {
+                    resultDiv.append("No results found.")
+                } else {
+                    resultDiv.appendChild(getAllWordTokens(searchString, wordCheck[1], item.total_count, item.verse_addresses, item.verse_tokens));
+                }
             }
         }
     }).catch(err => console.error(err))
