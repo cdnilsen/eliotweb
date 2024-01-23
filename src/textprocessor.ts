6786d141e165f0bfb566a32dc6a629e649755c98
unknown <campbell.nilsen@gmail.com>
2024-01-23 10:38:12 -0500

    maybe this works?

--- src/textprocessor.ts
+++ src/textprocessor.ts
@@ -7,18 +7,18 @@ import { wrapAsync } from './utils'
 const app = express()
 const port = process.env.PORT
 
+app.get('/processText', wrapAsync(async (req, res) => {
+    const words = await pool.query('SELECT * FROM test_table')
+    res.json(words.rows)
+}))
+
 app.get('/fetchBook/:book/:edition', wrapAsync(async (req, res) => {
-    const book = req.query.book;
-    const edition = req.query.edition;
+    const book = req.params["book"];
+    const edition = req.params["edition"];
     let textAddress = "./texts/" + book + "." + edition + ".txt";
 
     let bookObject = await fetch(textAddress);
     res.json(bookObject);
-}))
-
-app.get('/processText', wrapAsync(async (req, res) => {
-    const words = await pool.query('SELECT * FROM test_table')
-    res.json(words.rows)
 }))
 
 app.use(express.static('public'))
