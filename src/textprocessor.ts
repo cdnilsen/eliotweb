07cb5cb0d2433d1d5886d1760930b35a986d7fd2
unknown <campbell.nilsen@gmail.com>
2024-01-24 12:48:12 -0500

    baby steps for the word-count database

--- src/textprocessor.ts
+++ src/textprocessor.ts
@@ -14,20 +14,32 @@ function cleanPunctuation(word: string) {
     return cleanWord;
 }
 
+type wordToIntDictType = {
+    [key: string]: number
+}
+
 function getWordsInText(verseText: string) {
-    let finalList: string[] = [];
+    let finalWordList: string[] = [];
+    let finalCountDict: wordToIntDictType = {};
+    let finalCountList: number[] = [];
     let wordList = verseText.split(" ");
     for (let i = 0; i < wordList.length; i++) {
         let cleanWord = cleanPunctuation(wordList[i]);
-        if (cleanWord.length > 0 && !finalList.includes(cleanWord.toLowerCase())) {
-            finalList.push(cleanWord.toLowerCase());
+        if (cleanWord.length > 0 && !finalWordList.includes(cleanWord.toLowerCase())) {
+            finalWordList.push(cleanWord.toLowerCase());
+            finalCountDict[cleanWord.toLowerCase()] = 1;
+        } else if (cleanWord.length > 0) {
+            finalCountDict[cleanWord.toLowerCase()] += 1;
         }
+    }
+    for (let i = 0; i < finalWordList.length; i++) {
+        finalCountList.push(finalCountDict[finalWordList[i]]);
     }
     // This probably doesn't work (yet)
     // return finalList.toSorted(Intl.Collator().compare);
-    return finalList;
+    return finalCountDict;
 }
-
+//Rename this string to string type or something
 type editionToColumnType = {
     [key: string]: string
 }
@@ -47,17 +59,122 @@ const editionToWordListDict: editionToColumnType = {
     "Zeroth Edition": "words_diacritics_zeroth_ed"
 };
 
+function killDiacritics(word: string) {
+    let charReplacementDict: editionToColumnType = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u",
+        "à": "a",
+        "è": "e",
+        "ì": "i",
+        "ò": "o",
+        "ù": "u",
+        "â": "a",
+        "ê": "e",
+        "î": "i",
+        "ô": "o",
+        "û": "u",
+        "ä": "a",
+        "ë": "e",
+        "ï": "i",
+        "ö": "o",
+        "ü": "u",
+        "ã": "a",
+        "õ": "o",
+        "ñ": "nn",
+        "m̃": "mm",
+        "ũ": "u",
+        "ẽ": "e",
+        "ĩ": "i",
+        "ā": "an",
+        "ē": "en",
+        "ī": "in",
+        "ō": "on",
+        "ū": "un"
+    }
 
-async function updateEdition(verseExists: boolean, verseID: string, verseText: string, edition: string, book: string, consoleAddress: string, editionColumn: string, diacriticWordListColumn: string, wordList: string[], chapter: number, verse: number) {
+    let cleanedWord = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] in charReplacementDict) {
+            cleanedWord += charReplacementDict[word[i]];
+        } else {
+            cleanedWord += word[i];
+        }
+    }
+    return cleanedWord;
+}
+
+async function updateOneWordTable(verseIDNum: number, editionNum: number, word: string, wordCount: number, useDiacritics: boolean) {
+
+    let whichTable = "";
+    if (useDiacritics) {
+        whichTable = "words_diacritics";
+    } else {
+        whichTable = "words_no_diacritics";
+    }
+
+    let checkQuery = await pool.query('SELECT * FROM ' + whichTable + ' WHERE word = $1', [word]);
+
+    let hasWord = (checkQuery.rows.length > 0);
+
+    if (hasWord) {
+        //await pool.query('UPDATE ' + whichTable + ' SET total_count = total_count + $1 WHERE word = $2', [wordCount, word]);
+    } else {
+        await pool.query('INSERT INTO ' + whichTable + '(word) VALUES($1)', [word]);
+    }
+
+}
+
+async function updateWordTables(verseID: string, edition: string, wordList: string[], wordCountList: number[]) {
+
+    //Prime numbers make it easy to check which editions a word is in
+    const editionToNumDict: editionToColumnType = {
+        "First Edition": "2",
+        "Second Edition": "3",
+        "Mayhew": "5",
+        "Zeroth Edition": "7"
+    };
+    let editionNum = editionToNumDict[edition];
+    let verseIDNum = parseInt(editionNum + verseID);
+    let diacriticsDict: wordToIntDictType = {};
+    let noDiacriticsDict: wordToIntDictType = {};
+    for (let i = 0; i < wordList.length; i++) {
+        diacriticsDict[wordList[i]] = wordCountList[i];
+        let wordCleaned = killDiacritics(wordList[i]);
+        if (wordCleaned in noDiacriticsDict) {
+            noDiacriticsDict[wordCleaned] += wordCountList[i];
+        } else {
+            noDiacriticsDict[wordCleaned] = wordCountList[i];
+        }
+    }
+
+    let diacriticsList = Object.keys(diacriticsDict);
+    let noDiacriticsList = Object.keys(noDiacriticsDict);
+
+    for (let i = 0; i < diacriticsList.length; i++) {
+        await updateOneWordTable(verseIDNum, parseInt(editionNum), diacriticsList[i], diacriticsDict[diacriticsList[i]], true);
+    }
+    for (let j = 0; j < noDiacriticsList.length; j++) {
+        await updateOneWordTable(verseIDNum, parseInt(editionNum), noDiacriticsList[j], noDiacriticsDict[noDiacriticsList[j]], false);
+    }
+}
+
+
+async function updateEdition(verseExists: boolean, verseID: string, verseText: string, edition: string, book: string, consoleAddress: string, editionColumn: string, diacriticWordListColumn: string, wordList: string[], wordCountList: number[], chapter: number, verse: number) {
 
     let isMassachusett: boolean = (edition == "First Edition" || edition == "Second Edition" || edition == "Mayhew" || edition == "Zeroth Edition");
 
     if (isMassachusett && verseExists) {
         let queryText = "UPDATE all_verses SET " + editionColumn + " = $1, " + diacriticWordListColumn + " = $2 WHERE id = $3";
         await pool.query(queryText, [verseText, wordList, parseInt(verseID)])
+
+        await updateWordTables(verseID, edition, wordList, wordCountList);
         return (consoleAddress + " updated in database.")
     } else if (isMassachusett && !verseExists) {
         await pool.query('INSERT INTO all_verses(id, book, ' + editionColumn + ', ' + diacriticWordListColumn + ' chapter, verse) VALUES($1, $2, $3, $4, $5, $6)', [parseInt(verseID), book, verseText, wordList, chapter, verse]);
+        await updateWordTables(verseID, edition, wordList, wordCountList);
         return (consoleAddress + " inserted into database.")
     } else if (!isMassachusett && verseExists) {
         let queryText = "UPDATE all_verses SET " + editionColumn + " = $1 WHERE id = $2";
@@ -85,13 +202,18 @@ async function verseUpdate(verseExists: boolean, verseID: string, verseText: str
         verse = parseInt(verseID.slice(6));
         consoleAddress = edition + " " + book + " " + verseID.slice(4, 6) + ":" + verseID.slice(6);
     }
+    
     let wordList: string[] = [];
-
+    let wordCountList: number[] = [];
     if (edition == "First Edition" || edition == "Second Edition" || edition == "Mayhew" || edition == "Zeroth Edition") {
-        wordList = getWordsInText(verseText);
+        let wordTextsAndCountDict = getWordsInText(verseText);
+        wordList = Object.keys(wordTextsAndCountDict);
+        for (let i = 0; i < wordList.length; i++) {
+            wordCountList.push(wordTextsAndCountDict[wordList[i]]);
+        }
     }
 
-    let outcome = await updateEdition(verseExists, verseID, verseText, edition, book, consoleAddress, editionColumn, diacriticWordListColumn, wordList, chapter, verse);
+    let outcome = await updateEdition(verseExists, verseID, verseText, edition, book, consoleAddress, editionColumn, diacriticWordListColumn, wordList, wordCountList, chapter, verse);
     return outcome;
 }
 
