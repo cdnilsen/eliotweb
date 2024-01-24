2bba6c051c9f437b2331bcb32ea6aa5395e0a725
unknown <campbell.nilsen@gmail.com>
2024-01-24 10:11:14 -0500

    trying again...

--- src/textprocessor.ts
+++ src/textprocessor.ts
@@ -27,9 +27,10 @@ async function verseUpdate(verseExists: boolean, verseID: string, verseText: str
         //return "verse exists in the db"
         let queryText = "UPDATE all_verses SET " + editionColumn + " = $1 WHERE id = $2";
         await pool.query(queryText, [verseText, parseInt(verseID)])
+        return (verseID + " updated in database.")
     } else {
         //return "verse does not exist in the db"
-        await pool.query('INSERT INTO all_verses(id, book, ' + editionColumn + ') VALUES($1, $2, $3,)', [parseInt(verseID), book, verseText]);
+        await pool.query('INSERT INTO all_verses(id, book, ' + editionColumn + ') VALUES($1, $2, $3)', [parseInt(verseID), book, verseText]);
         return (verseID + " inserted into database.")
     }
 }
@@ -41,11 +42,11 @@ export async function processVerseJSON(rawJSON: any) {
     let book = rawJSON.book;
     let edition = rawJSON.edition;
     let columnString = editionToColumnDict[edition];
+    //This works.
     let myQuery = await pool.query('SELECT * FROM all_verses WHERE id = $1', [parseInt(idNumber)]);
-
     let hasVerse = (myQuery.rows.length > 0);
-    return hasVerse;
     //if myQuery.rows.length > 0, then the verse already exists in the database and we want to pass `true` to 'verseExists' in verseUpdate
+
     let returnValue = await verseUpdate(hasVerse, idNumber, rawText, columnString, book);
 
     return returnValue;
