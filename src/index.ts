6e4b6a720ef45f1bed333de4442303a6a24fb220
unknown <campbell.nilsen@gmail.com>
2024-01-26 12:46:17 -0500

    different parameters?

--- src/index.ts
+++ src/index.ts
@@ -81,9 +81,9 @@ app.put('/runWordCounts', wrapAsync(async (req, res) => {
 
 app.get('/fetchVerse', wrapAsync(async (req, res) => {
     try {
-        let verseID: number = parseInt(req.body["verseID"]);
-        let editionNumber: number = parseInt(req.body["editionNumber"]);
-        let useRawText: boolean = (req.body["useRawText"] === 'true');
+        let verseID: number = parseInt(req.body.verseID);
+        let editionNumber: number = parseInt(req.body.editionNumber);
+        let useRawText: boolean = (req.body.useRawText === 'true');
 
         let verseTextDict = await getVerseText(verseID, useRawText, editionNumber);
 
