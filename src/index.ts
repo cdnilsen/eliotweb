cb2cf97fdaa6f84ed65c8b4503ab574ec3265a4c
unknown <campbell.nilsen@gmail.com>
2024-01-27 11:44:12 -0500

    hmm

--- src/index.ts
+++ src/index.ts
@@ -96,7 +96,7 @@ app.get('/fetchVerse/:verseID/:editionNumber/:useRawString', wrapAsync(async (re
         /*
         let verseTextDict = await getVerseText(verseID, useRawString, editionNumber);
         */
-        let result: string = "fetchVerse called at verse " + verseID.toString() + " in edition " + editionNumber.toString() + " with useRawString = " + req.params.useRawString.toString() + ".";
+        let result: string = "fetchVerse called at verse " + verseID.toString() + " in edition " + editionNumber.toString() + " with useRawString = " + req.params.useRawString + ".";
         res.json(result);
 
         //res.json((verseID.toString() + ": " + editionNumber.toString() + ": " + useRawText.toString()));
