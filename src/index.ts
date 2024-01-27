ad8d61afcfa8ad0049d90cb45bf55aaa8a90a097
unknown <campbell.nilsen@gmail.com>
2024-01-27 12:34:24 -0500

    checking something

--- src/index.ts
+++ src/index.ts
@@ -92,11 +92,11 @@ app.get('/fetchVerse/:verseID/:editionNumber/:useRawString', wrapAsync(async (re
         
         let editionNumber: number = parseInt(req.params.editionNumber);
         
-        //let useRawString: boolean = (req.params.useRawString == 'true');
+        let useRawString: boolean = (req.params.useRawString == 'tfrue');
         /*
         let verseTextDict = await getVerseText(verseID, useRawString, editionNumber);
         */
-        let result: string = "fetchVerse called at verse " + verseID.toString() + " in edition " + editionNumber.toString() + " and | " + req.params.useRawString + ".";
+        let result: string = "fetchVerse called at verse " + verseID.toString() + " in edition " + editionNumber.toString() + " and | " + useRawString.toString() + ".";
         res.json(result);
 
         //res.json((verseID.toString() + ": " + editionNumber.toString() + ": " + useRawText.toString()));
