50f3ef15e53594f8ab8e35d33cd7366996458dcd
unknown <campbell.nilsen@gmail.com>
2024-01-27 15:50:05 -0500

    works?!

--- public/browseTexts.js
+++ public/browseTexts.js
@@ -378,7 +378,7 @@ async function getOneVerseText(book, chapter, verse, useFirst, useSecond, useMay
             let prime = primeNumbers[i];
             if (editionNumber % prime == 0) {
                 let span = document.createElement('span');
-                span.innerHTML = res[prime] + '\n';
+                span.innerHTML = res[prime.toString()] + '\n';
                 textContainer.appendChild(span);
             }
         }
