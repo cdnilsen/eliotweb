ad668318e567a79fe3f835c11c62cea75f64d998
unknown <campbell.nilsen@gmail.com>
2024-02-01 18:27:00 -0500

    compare words

--- src/index.ts
+++ src/index.ts
@@ -64,7 +64,8 @@ app.post('/processWords', wrapAsync(async (req, res) => {
     }
 }));
 
-app.put('/compareWords/:verseID', wrapAsync(async (req, res) => {
+//change this to a PUT request
+app.get('/compareWords/:verseID', wrapAsync(async (req, res) => {
     try {
         let verseID: number = parseInt(req.params.verseID);
 
