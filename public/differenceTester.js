5766c9a582acbfd0c5e9dd75aa99485bdafde8bb
unknown <campbell.nilsen@gmail.com>
2024-02-06 13:10:45 -0500

    victory?

--- public/differenceTester.js
+++ public/differenceTester.js
@@ -605,10 +605,10 @@ submitButton.addEventListener("click", async function(event) {
         let finalStrings = getDifferences(firstEdText, secondEdText, chapterNum, verseNum);
 
         let div1 = document.createElement('div');
-        div1.innerHTML = firstEdText + "<br>" + finalStrings[0];
+        div1.innerHTML = replaceDummiesWithTags(finalStrings[0]);
 
         let div2 = document.createElement('div');
-        div2.innerHTML = secondEdText + "<br>" + finalStrings[1];
+        div2.innerHTML = replaceDummiesWithTags(finalStrings[1]);
 
         let verseSpan = document.createElement("span");
         verseSpan.innerHTML = "<u>" + verseNum.toString() + "</u><br>";
