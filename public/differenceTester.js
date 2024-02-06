3ae9384a9cd3b5817d880ccdb8e35c4e55a607e4
cdnilsen <49859468+cdnilsen@users.noreply.github.com>
2024-02-06 11:44:54 -0500

    wrong separators

--- public/differenceTester.js
+++ public/differenceTester.js
@@ -277,10 +277,10 @@ function replaceCommonSubstrings(text1, text2, index) {
     }
     
     if (text2.startsWith(commonSubstring)) {
-        outputText2 = '|«' + index.toString() + '»|' + text2.slice(commonSubstringLength);
+        outputText2 = 'ǂ«' + index.toString() + '»ǂ' + text2.slice(commonSubstringLength);
         //console.log(outputText2);
     } else {
-        outputText2 = text2Split.join('|«' + index.toString() + '»|');
+        outputText2 = text2Split.join('ǂ«' + index.toString() + '»ǂ');
     }
 
 
