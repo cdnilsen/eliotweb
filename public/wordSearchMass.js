795b130314606b7869da2b2e7ec534483a94a7e7
unknown <campbell.nilsen@gmail.com>
2024-02-11 10:09:35 -0500

    does the diacritic cleaner work?

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -1,4 +1,79 @@
-// Allows the user to search for matching words in the Massachusett texts and outputs a list of all their cites
+// Allows the user to search for matching words in the Massachusett texts and outputs a list of all their cites.
+
+
+//This function is an attempt to deal with the macra and tildes that Eliot uses to represent a following nasal
+function processEngma(string) {
+
+    let wordCopy = word;
+    //These will need to be dealt with manually, but we'll need to do that later
+    if (word.endsWith('ŋ')) {
+        //let chapterString = chapter.toString();
+        //let verseString = verse.toString();
+        wordCopy = word.slice(0, -1);
+        wordCopy = wordCopy + "Ŋ";
+    }
+    
+    let labialEngmaClusters = ['ŋb', 'ŋp', 'ŋm', 'ŋf'];
+
+    let replacementClusters = ['mb', 'mp', 'mm', 'mf'];
+
+    for (let i = 0; i < 4; i++) {
+        wordCopy = wordCopy.split(labialEngmaClusters[i]).join(replacementClusters[i]);
+    }
+
+    wordCopy = wordCopy.split('ŋ').join('n');
+
+    return wordCopy;
+}
+
+function cleanDiacritics(string) {
+
+    let charReplacementDict = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u",
+        "à": "a",
+        "è": "e",
+        "ì": "i",
+        "ò": "o",
+        "ù": "u",
+        "â": "a",
+        "ê": "e",
+        "î": "i",
+        "ô": "o",
+        "û": "u",
+        "ä": "a",
+        "ë": "e",
+        "ï": "i",
+        "ö": "o",
+        "ü": "u",
+        "ã": "aN",
+        "õ": "oN",
+        "ñ": "nn",
+        "m̃": "mm",
+        "ũ": "uN",
+        "ẽ": "eN",
+        "ĩ": "iN",
+        "ā": "aN",
+        "ē": "eN",
+        "ī": "iN",
+        "ō": "oN",
+        "ū": "uN"
+    };
+
+    let cleanedWord = "";
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] in charReplacementDict) {
+            cleanedWord += charReplacementDict[word[i]];
+        } else {
+            cleanedWord += word[i];
+        }
+    }
+    return processEngma(cleanedWord);
+}
+
 
 function seeAllWords(resultDiv, searchString, searchSetting) {
     resultDiv.innerText = "";
@@ -17,7 +92,7 @@ function seeAllWords(resultDiv, searchString, searchSetting) {
     }).catch(err => console.error(err))
 }
 
-document.getElementById("searchButton").addEventListener("click", ev => {
+document.getElementById("searchButton").addEventListener("click", async function() {
     let searchSetting = 1;
 
     if (document.getElementById("searchMassachusett").checked) {
@@ -39,17 +114,26 @@ document.getElementById("searchButton").addEventListener("click", ev => {
         searchSetting *= 7;
     }
 
-    if (document.getElementById("diacriticsLax").checked) {
-        searchSetting *= 17;
-    }
-    if (document.getElementById("diacriticsStrict").checked) {
-        searchSetting *= 19;
-    }
+    
 
     let query = document.getElementById("search_bar").value;
 
+    if (document.getElementById("diacriticsLax").checked) {
+        searchSetting *= 17;
+        query = cleanDiacritics(query);
+    }
+
     console.log(searchSetting);
     console.log(query);
+    /*
+    let fetchString = "/getWords/" + query + "/" + searchSetting.toString();
+
+    await fetch(fetchString).then(res => res.json()).then(res => {
+        for (let item of res) {
+            console.log(item.word);
+        }
+    }).catch(err => console.error(err))
+    */
 
 
 });
