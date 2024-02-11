281b2fb7f278e6e1f12621dee19b2609fc036349
unknown <campbell.nilsen@gmail.com>
2024-02-11 12:08:16 -0500

    check the types

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -1,4 +1,80 @@
 // Allows the user to search for matching words in the Massachusett texts and outputs a list of all their cites.
+const allBookList = [
+    "Genesis",
+    "Exodus",
+    "Leviticus",
+    "Numbers",
+    "Deuteronomy",
+    "Joshua",
+    "Judges",
+    "Ruth",
+    "1 Samuel",
+    "2 Samuel",
+    "1 Kings",
+    "2 Kings",
+    "1 Chronicles",
+    "2 Chronicles",
+    "Ezra",
+    "Nehemiah",
+    "Esther",
+    "Job",
+    "Psalms (prose)",
+    //"Psalms (metrical)", Will require a separate process
+    "Proverbs",
+    "Ecclesiastes",
+    "Song of Songs",
+    "Isaiah",
+    "Jeremiah",
+    "Lamentations",
+    "Ezekiel",
+    "Daniel",
+    "Hosea",
+    "Joel",
+    "Amos",
+    "Obadiah",
+    "Jonah",
+    "Micah",
+    "Nahum",
+    "Habakkuk",
+    "Zephaniah",
+    "Haggai",
+    "Zechariah",
+    "Malachi", 
+    "Matthew",
+    "Mark",
+    "Luke",
+    "John",
+    "Acts",
+    "Romans",
+    "1 Corinthians",
+    "2 Corinthians",
+    "Galatians",
+    "Ephesians",
+    "Philippians",
+    "Colossians",
+    "1 Thessalonians",
+    "2 Thessalonians",
+    "1 Timothy",
+    "2 Timothy",
+    "Titus",
+    "Philemon",
+    "Hebrews",
+    "James",
+    "1 Peter",
+    "2 Peter",
+    "1 John",
+    "2 John",
+    "3 John",
+    "Jude",
+    "Revelation"
+];
+
+editionToSuperscriptDict = {
+    "2": "<sup>α</sup>",
+    "3": "<sup>β</sup>",
+    "5": "<sup>M</sup>",
+    "7": "<sup>א</sup>",
+}
 
 
 //This function is an attempt to deal with the macra and tildes that Eliot uses to represent a following nasal
@@ -80,11 +156,17 @@ function decodeVerseCode(verseCode) {
 
 function getVerseCodeSpan(verseList, verseCount) {
     let verseCodeText = "";
+    let firstEditionDict = {};
+    let secondEditionDict = {};
+    let MayhewDict = {};
+    let ZerothEditionDict = {};
+
     for (let i = 0; i < verseList.length; i++) {
-        verseCodeText += verseList[i] + ", ";
-        if (i == verseList.length -1){
-            verseCodeText = verseCodeText.slice(0, -2);
-        }
+
+        console.log(typeof verseList[i])
+        console.log(typeof verseCount[i])
+        let verseCode = decodeVerseCode(verseList[i]);
+        let thisVerseCount = verseCount[i];
     }
     return verseCodeText;
 }
