6be07b8a91a327c4d39ad81a8f759be8daf1bf16
unknown <campbell.nilsen@gmail.com>
2024-02-13 17:28:31 -0500

    let's see...

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -752,7 +752,7 @@ function processAllWordCites(allWordList, dictOfDicts, sortAlphabetical) {
 
                 triangleSandwich(allBooksContainer, thisBookSpan, verseCiteContainer, verseTextList.length > 30, "red", false, true);
             }
-            triangleSandwich(headerResultsDiv, thisWordDiv, allBooksContainer, allBookNums.length > 5, "#00FF50", false, false);
+            triangleSandwich(headerResultsDiv, thisWordDiv, allBooksContainer, allBookNums.length > 5, "#00FF50", true, allBookNums.length <= 5);
         }
         triangleSandwich(resultDiv, thisHeaderDiv, headerResultsDiv, true, "blue", false, false);
     }
