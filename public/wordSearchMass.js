a6f182ea5015741fac56439c36feaa592be631f3
unknown <campbell.nilsen@gmail.com>
2024-02-13 17:50:48 -0500

    git commit -m "i think this should work quite well?"

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -751,9 +751,9 @@ function processAllWordCites(allWordList, dictOfDicts, sortAlphabetical) {
                 }
                 */
 
-                triangleSandwich(allBooksContainer, thisBookSpan, verseCiteContainer, verseTextList.length > 30, "red", false, allBookNums.length <= 5);
+                triangleSandwich(allBooksContainer, thisBookSpan, verseCiteContainer, verseTextList.length > 30, "red", false, false);
             }
-            triangleSandwich(headerResultsDiv, thisWordDiv, allBooksContainer, allBookNums.length > 5, "#00FF50", false, true);
+            triangleSandwich(headerResultsDiv, thisWordDiv, allBooksContainer, allBookNums.length > 5, "#00FF50", false, false);
         }
         triangleSandwich(resultDiv, thisHeaderDiv, headerResultsDiv, true, "blue", false, false);
     }
