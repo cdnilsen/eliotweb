f1338b9281930b61c3f00b16c79d15be772dfb2a
unknown <campbell.nilsen@gmail.com>
2024-02-17 11:34:43 -0500

    ???

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -416,7 +416,7 @@ async function showVersesInBox(popupContainer, dbCode, book, activeWord, laxDiac
         popupContainer.appendChild(table);
         popupContainer.classList.toggle('active');
     });
-    popupContainer.style.position = "static";
+    popupContainer.style.position = "relative";
     popupContainer.style.top = (verticalCoord - 50).toString() + "px";
     console.log(verticalCoord);
     popupContainer.style.left = "30%";
