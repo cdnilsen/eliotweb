c16f7f4cc67c6da8e624d2c1397dc212bb1630f8
unknown <campbell.nilsen@gmail.com>
2024-02-18 18:01:52 -0500

    hmm

--- public/wordSearchMass.js
+++ public/wordSearchMass.js
@@ -789,10 +789,13 @@ function addVersesToContainer(verseTextList, dbCodeList, word, book, topDiv, lax
         let popupContainer = document.createElement("span");
         
         thisVerseSpan.addEventListener("click", async function() {
+            console.log(otherVerseSpanList.length);
+            /*
             for (let i=0; i < otherVerseSpanList.length; i++) {
                 otherVerseSpanList[i].style.color = "black";
                 otherVerseSpanList[i].classList("active") = false;
             }
+            */
             thisVerseSpan.classList.toggle('active');
             await showVersesInBox(popupContainer, thisDBCode, book, word, laxDiacritics);
             thisVerseSpan.style.color = "blue";
