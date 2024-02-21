6180973ef7a8fa2468072968e7f2f357ad959bfc
unknown <campbell.nilsen@gmail.com>
2024-02-20 19:20:39 -0500

    indentation?

--- public/wordSearchEnglish.js
+++ public/wordSearchEnglish.js
@@ -311,7 +311,7 @@ function processAllWordCites(allWordList, dictOfDicts, sortAlphabetical, laxDiac
                 let verseCiteContainer = addVersesToContainer(verseTextList, allDBCodes, thisWord, thisBookName, laxDiacritics);
 
                 if (verseTextList.length > 25){                    
-
+                    verseCiteContainer.style.display = "inline-block";
                     let bookTriangle = addTriangleToParent(thisBookSpan, "gray", "red", true);
 
                     let breakSpan1 = document.createElement("br");
